21dbcab6a07b1516bd20b99c271dbb2fd8d56aab
Carlos_Dias <carloseduardotupa19@gmail.com>
2023-09-25 18:50:13 -0300

    commitSkills

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ export default{
     data: function () {
         return {
             character: null,
+            name: null,
+            skill: null
         }
     },
     computed: {
@@ -12,9 +14,10 @@ export default{
         getDados(){
             fetch("https://valorant-api.com/v1/agents").then(res => { res.json().then((x) =>{ this.character = x.data })})
         },
-        teste(c){
+        infos(c){
+            this.name = c.displayName
+            this.skill = c.abilities
             console.log(c)
-            this.abreModal();
         },
         abreModal(){
             
@@ -28,7 +31,6 @@ export default{
 </script>
 
 <template>
-
   <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
     <div class="bg-dark p-4">
       <h5 class="text-body-emphasis h4">Usando API do valorant com vue js.</h5>
@@ -42,6 +44,7 @@ export default{
       </button>
     </div>
   </nav>
+
   <div class="container texte-center">
     <div class="row">
         <div class="row col-12">
@@ -56,7 +59,7 @@ export default{
                             <div>
                                 <p class="mt-4"> <strong>Description:</strong> {{ c.description }}</p>
                             </div>
-                            <button type="button" @click="teste(c)" class="btn btn-dark">Info Skills</button>
+                            <button @click="infos(c)" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Info Skills</button>
                         </div>
                     </div>
                 </div>
@@ -65,26 +68,44 @@ export default{
     </div>
   </div>
 
-  <div id="myModal" class="modal" tabindex="-1" role="dialog">
-  <div  class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <div style="background-color:cadetblue;" class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Skills</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div style="background-color: cadetblue;" class="modal-body">
+            <div class="row">
+              <div class="col-md-5"></div>
+              <div>
+                <h1>{{this.name}}</h1>
+            </div>
+            <div v-for="skill in this.skill" class="col-6 d-flex justify-content-center">
+              <div class="col-sm-12 col-md-6">
+                    <div id="card" class="card">
+                        <div style="background-color: #d7f7f4;" class="card-body row">
+                            <div style="" class=""><strong>{{ skill.displayName }}</strong></div>
+                            <div>
+                                <img :src="skill.displayIcon">
+                            </div>
+                            <div>
+                                <p class="mt-4"> <strong>Description:</strong> {{ skill.description }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div style="background-color:cadetblue;" class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
 
+</div>
 </template>
 <style scoped>
 </style>
